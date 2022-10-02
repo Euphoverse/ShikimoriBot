4325214df93fc4c62583e40c2f81b9f4d3a3a6cb
Filo6699 <karka2-6@rambler.ru>
2022-10-02 13:58:38 +0600

    Small fix

--- shiki/extensions/main/economy.py
+++ shiki/extensions/main/economy.py
@@ -27,7 +27,6 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from datetime import datetime, timedelta
-from nturl2path import url2pathname
 import random
 import lightbulb
 import hikari
