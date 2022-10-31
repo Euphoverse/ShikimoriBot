6e91b461aa2fc604d92376ab506b3ec29f81f482
Filo6699 <karka2-6@rambler.ru>
2022-10-31 18:46:29 +0600

    Deleted 1 achievement

--- shiki/extensions/achievements/statistic.py
+++ shiki/extensions/achievements/statistic.py
@@ -125,9 +125,6 @@ async def state_update(event: hikari.VoiceStateUpdateEvent):
         if s > 3_240_000:
             await tools.grant_achievement(state.user_id, 
                                     '9', plugin.app.rest)
-
-    if state.channel_id == cfg[cfg['mode']]['channels']['radio']:
-        await tools.grant_achievement(event.state.user_id, '35', plugin.bot.rest)
     
     if state.is_streaming == True:
         await tools.grant_achievement(event.state.user_id, '36', plugin.bot.rest)
